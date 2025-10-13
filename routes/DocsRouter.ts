746065dcb21cd7012de9c66ebe1244769f89a6d6
MathGueff <gueffmatheus@gmail.com>
2025-10-13 16:11:50 -0300

    fix: swagger routes

--- routes/DocsRouter.ts
+++ routes/DocsRouter.ts
@@ -6,7 +6,7 @@ const DocsRouter = Router();
 const apiExpressReservation = new Swagger({
   title: "API Reserva Express",
   version: "1.0.0",
-  routerPaths: ["./features/user/UserRouter.ts", "./features/auth/AuthRouter.ts", "./features/reservation/ReservationRouter.ts"],
+  routerPaths: ["./features/api/user/UserRouter.ts", "./features/api/auth/AuthRouter.ts", "./features/api/reservation/ReservationRouter.ts"]
 });
 
 DocsRouter.use(
